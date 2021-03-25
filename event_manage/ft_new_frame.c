7e4609b0b1c4a49e4a49c015d275cad12cf8d65c
Lodovico Spazzini <lspazzin@e3r3p3.42roma.it>
2021-03-25 12:45:51 +0100

    normed

--- event_manage/ft_new_frame.c
+++ event_manage/ft_new_frame.c
@@ -6,7 +6,7 @@
 /*   By: lspazzin <lspazzin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/24 14:28:29 by lodovico          #+#    #+#             */
-/*   Updated: 2021/03/25 11:19:40 by lspazzin         ###   ########.fr       */
+/*   Updated: 2021/03/25 12:45:26 by lspazzin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int		ft_new_frame(t_param *param)
 	ft_movement(param);
 	param->img = ft_img_init(param);
 	ft_img_fill(param);
-//	mlx_clear_window(param->mlx, param->win);
+	mlx_clear_window(param->mlx, param->win);
 	mlx_put_image_to_window(param->mlx, param->win, param->img->ptr, 0, 0);
 	mlx_destroy_image(param->mlx, param->img->ptr);
 	free(param->img);
